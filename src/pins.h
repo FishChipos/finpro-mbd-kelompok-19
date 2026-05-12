2480b48f1913d68b2736d2a54fc431b86f680804
Carlsson Khovis <carlkho99@gmail.com>
2026-05-12 17:56:31 +0700

    Added framework for load cells with HX711 amplifier.

---
+++ src/pins.h
@@ -0,0 +1,7 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define PIN_HX711_DOUT 0
+#define PIN_HX711_PD_SCK 0
+
+#endif
---
+++ src/variables.h
@@ -0,0 +1,6 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#define VAR_CALIBRATION_FACTOR 0x100
+
+#endif
